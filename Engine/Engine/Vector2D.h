a2215ffe50225509be629ebd3a5101afe8ab8dc5
Danny Huynh <danhuynh0803@gmail.com>
2016-05-31 03:45:15 -0400

    Refactored vector2d definitions to be in a .inl header file. Declarations are inlines within the vector2d.h

--- Engine/Engine/Vector2D.h
+++ Engine/Engine/Vector2D.h
@@ -11,20 +11,9 @@ namespace Math
 		Vector2D(float x = 0.0f, float y = 0.0f) : x(x), y(y) {}
 	};
 
-	Vector2D operator+(const Vector2D& left, const Vector2D& right)
-	{
-		return Vector2D();
-	}
-
-	Vector2D operator*(float scalar, const Vector2D& vector)
-	{
-		return Vector2D();
-	}
-
-	Vector2D operator*(const Vector2D& vector, float scalar)
-	{
-		return Vector2D();
-	}
-
+	inline Vector2D operator+(const Vector2D& left, const Vector2D& right);
+	inline Vector2D operator*(float scalar, const Vector2D& vector);
+	inline Vector2D operator*(const Vector2D& vector, float scalar);
+#include "vector2D.inl"
 }
 #endif
--- EngineTester/Vector2DTests.cpp
+++ EngineTester/Vector2DTests.cpp
@@ -12,10 +12,24 @@ TEST(Vector2D, Constructor)
 
 TEST(Vector2D, VectorAddition)
 {
-
+	Vector2D first(1, 2);
+	Vector2D second(3, 4);
+	Vector2D result1 = first + second; 
+	Vector2D result2 = second + first;
+	EXPECT_FLOAT_EQ(4, result1.x);
+	EXPECT_FLOAT_EQ(6, result1.y);
+	EXPECT_FLOAT_EQ(result2.x, result1.x);
+	EXPECT_FLOAT_EQ(result2.y, result1.y);
 }
 
-TEST(Vector2D, VectorMultiplication)
+TEST(Vector2D, ScalarMultiplication)
 {
-
+	Vector2D vec(1, 2); 
+	float scalar = 2; 
+	Vector2D result1 = vec * scalar;
+	Vector2D result2 = scalar * vec;
+	EXPECT_FLOAT_EQ(result1.x, 2);
+	EXPECT_FLOAT_EQ(result1.y, 4);
+	EXPECT_FLOAT_EQ(result1.x, result2.x);
+	EXPECT_FLOAT_EQ(result1.y, result2.y);
 }
